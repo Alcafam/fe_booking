8e27275d9d315b676f0ab11b415e2f2ca9dfd5c6
AlcaFam <galinganmarjy61@gmail.com>
2025-03-27 04:44:03 +0800

    fixes

--- app/components/layouts/layout.tsx
+++ app/components/layouts/layout.tsx
@@ -7,7 +7,7 @@ export default function ProjectLayout() {
             <div className="container-fluid">
                 <nav className="navbar navbar-light bg-light justify-content-start border border-0">
                 <div className="form-inline position-absolute end-0 px-5">
-                <ul className="list-unstyled"><li className="fs-3"><a href = "/login">Login</a>/<a href = "/register">Register</a></li></ul>
+                <ul className="list-unstyled"><li className="fs-3"><a href = "/">Login</a>/<a href = "/register">Register</a></li></ul>
                 </div>
                 </nav>
             </div>
