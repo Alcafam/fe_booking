d3c1488e6e15477fab55e06fdb5a04c00148680e
AlcaFam <galinganmarjy61@gmail.com>
2025-06-02 18:10:24 +0800

    notes

--- app/components/auth/register.tsx
+++ app/components/auth/register.tsx
@@ -137,6 +137,25 @@ const RegistrationForm = () => {
                         <p className="text-center fs-5">Already have an account?</p>
                         <Link to = "/" className="fs-5">Login</Link>
                     </form>
+
+                    <div class="bg-warning container mt-5 p-3 rounded">
+                        <b>Note:</b><br>
+                        For testing purposes, you can use the following accounts:
+                        <ul class="mt-2">
+                            <li><b>Admin Account</b><br>
+                                Email: <code>alice.johnson@example.com</code><br>
+                                Password: <code>password</code>
+                            </li>
+                            <li class="mt-2"><b>User Account 1</b><br>
+                                Email: <code>james.williams@example.com</code><br>
+                                Password: <code>password</code>
+                            </li>
+                            <li class="mt-2"><b>User Account 2</b><br>
+                                Email: <code>sarah.miller@example.com</code><br>
+                                Password: <code>password</code>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
                 </div>
         </div>
