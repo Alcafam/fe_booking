681a40904863670c845237694f84c201400b8e55
AlcaFam <galinganmarjy61@gmail.com>
2025-06-02 18:23:24 +0800

    class to className fix

--- app/components/auth/login.tsx
+++ app/components/auth/login.tsx
@@ -118,25 +118,34 @@ const LoginForm = () => {
             </form>
           </div>
         </div>
-        <div class="bg-warning container mt-5 p-3 rounded">
-              <b>Note:</b><br/>
-              For testing purposes, you can use the following accounts:
-              <ul class="mt-2">
-                  <li><b>Admin Account</b><br/>
-                      Email: <code>alice.johnson@example.com</code><br/>
-                      Password: <code>password</code>
-                  </li>
-                  <li class="mt-2"><b>User Account 1</b><br/>
-                      Email: <code>james.williams@example.com</code><br/>
-                      Password: <code>password</code>
-                  </li>
-                  <li class="mt-2"><b>User Account 2</b><br/>
-                      Email: <code>sarah.miller@example.com</code><br/>
-                      Password: <code>password</code>
-                  </li>
-              </ul>
-          </div>
-      </div>
+        <div className="bg-warning container mt-5 p-3 rounded">
+          <b>Note:</b>
+          <br />
+          For testing purposes, you can use the following accounts:
+          <ul className="mt-2">
+            <li>
+              <b>Admin Account</b>
+              <br />
+              Email: <code>alice.johnson@example.com</code>
+              <br />
+              Password: <code>password</code>
+            </li>
+            <li className="mt-2">
+              <b>User Account 1</b>
+              <br />
+              Email: <code>james.williams@example.com</code>
+              <br />
+              Password: <code>password</code>
+            </li>
+            <li className="mt-2">
+              <b>User Account 2</b>
+              <br />
+              Email: <code>sarah.miller@example.com</code>
+              <br />
+              Password: <code>password</code>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
